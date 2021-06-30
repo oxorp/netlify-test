b55609e029c40d84f295729270c377ddb07b4f0d
Robert Hartkopf <github@hrmh.dev>
2021-06-30 17:09:22 +0700

    hello leon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h3>
-          HALLO TIM! :3
+          HALLO LEON! :3
         </h3>
       
       </header>
